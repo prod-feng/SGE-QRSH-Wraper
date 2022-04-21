4cc841cc62b5c2b54835ed1241a9cba5324350ac
Feng Zhang <prod.feng@gmail.com>
2022-04-21 11:40:17 -0400

    Update qrsh_wraper.sh

--- qrsh_wraper.sh
+++ qrsh_wraper.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-
+#
+#A wraper script to use qrsh to run interactive jobs, while setting the env variables
+#like a batch job, eg, JOB_ID, NSLOTS, CUDA_VISIBLE_DEVICE, etc.
+#
+#For ssh app used for rsh_command, and rsh_daemon, add "-t" option to force the 
+#pseudo-terminal allocation.
+#
+#rsh_command                  /usr/bin/ssh -t -o LogLevel=ERROR
+#rsh_daemon                   /usr/sbin/sshd -i
+#
+#-pty y option is for qrsh to force pseudo-terminal allocation. If use ssh as backend, it has no effect then.
+#
 qrsh -V -pty y "$@" $SHELL
